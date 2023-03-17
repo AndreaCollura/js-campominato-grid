a55f4381ff4027efd50a0a7380dd50c6ff6dc36d
AndreaCollura <andrea.collur@gmail.com>
2023-03-17 20:47:28 +0100

    add bomb found function

--- js/script.js
+++ js/script.js
@@ -22,11 +22,15 @@ difForm.addEventListener('submit', play);
 
 function play(e){
 
-    e.preventDefault();
 
+    e.preventDefault();
+    const playGround = document.getElementById('playground');
+    playGround.innerHTML = '';
     const userSelec = document.getElementById('level').value;
     console.log(userSelec); 
     let squareNumb ;
+    const bombNumb = 16;
+    
 
     switch (userSelec){
         case 'easy':
@@ -39,9 +43,30 @@ function play(e){
             squareNumb = 49;
             break;
     } 
+    const bombList = randomListGen(bombNumb, squareNumb);
+    const prList = []
+    
+    
+    for (let i = 1; i <= squareNumb; i++ ){
+        const square = boxGen(squareNumb, i);
 
-    const playGround = document.getElementById('playground');
+   /*  devo aggiungere al quadrato l'evento click assegnando una classe se é una bomba o é salvo */
+       square.addEventListener('click', function(){
+        // come faccio a stabilire quali quadrati sono bombe e quali sono safe?
+        // cosa so io dei quadrati? conosco il contenuto = i + il numero dei quadrati
+        // sostituisco il numero fisso con la costante creata sopra
+        // controllo se i numeri dentro l'array é uguale al l'indice 
 
+        
+        if (prList.indexOf(i) > -1  ){
+        // aggiungo classe bomba
+        }
+       } ) 
+
+        
+
+        playGround.appendChild(square);       
+    }
 
 }
 
@@ -55,14 +80,20 @@ il valore del quadrato  */
 
 function boxGen(squareNumb, content){
 
-   const square = document.createElement('div');
-   
+    let squareRoot = Math.sqrt(squareNumb);
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `calc(100% / ${squareRoot} )`;
+    square.style.height = `calc(100% / ${squareRoot} )`;
+    square.innerText = content;
 
-    
-    
+    return square;
+
 
 }
 
 
-
+function randomListGen(listLenght, numMaX){
+    const bombList =  [];
+}
 
